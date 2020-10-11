3ae92c20257e3cc28dbc3fa1be7b54a37c5fbf29
Baruch <baruch.roman@gmail.com>
2020-10-11 08:10:12 -0500

    Fin 104 - Delete: Marcar una eliminación en el mismo registro

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -16,7 +16,7 @@ app.get('/usuario', function(req, res) {
     let limite = req.query.limite || 5;
     limite = Number(limite);
 
-    Usuario.find({}, 'nombre email role estado google img')
+    Usuario.find({ estado: true }, 'nombre email role estado google img')
         .skip(desde)
         .limit(limite)
         .exec((err, usuarios) => {
@@ -28,7 +28,7 @@ app.get('/usuario', function(req, res) {
                 });
             }
 
-            Usuario.countDocuments({}, (err, conteo) => {
+            Usuario.countDocuments({ estado: true }, (err, conteo) => {
 
                 res.json({
                     ok: true,
@@ -97,7 +97,11 @@ app.delete('/usuario/:id', function(req, res) {
 
     let id = req.params.id;
 
-    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+    let cambiaEstado = {
+        estado: false
+    };
+
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
 
         if (err) {
             return res.status(400).json({
