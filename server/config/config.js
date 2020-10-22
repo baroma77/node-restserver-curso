dcab495d924c1901d079ce70fad6077b39e0855d
Baruch <baruch.roman@gmail.com>
2020-10-22 07:49:40 -0500

    Google Sign-In

--- server/config/config.js
+++ server/config/config.js
@@ -35,3 +35,8 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
+
+//==================
+//  Google Client ID
+//==================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '771908567775-9uf8fp0ss8vuh54sti6foc5lm43lthjp.apps.googleusercontent.com';
